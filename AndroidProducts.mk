3e05e66b463c082d2943c2b167184bcab0dc193f
JoseXda <josegustavo6@protonmail.com>
2023-11-03 12:34:32 -0300

    rhode: AfterLife BringUp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rhode.mk
+    $(LOCAL_DIR)/afterlife_rhode.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rhode-user \
-    lineage_rhode-userdebug \
-    lineage_rhode-eng
+    afterlife_rhode-user \
+    afterlife_rhode-userdebug \
+    afterlife_rhode-eng
